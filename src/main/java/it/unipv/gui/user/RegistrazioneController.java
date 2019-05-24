362c8191c19c22d0c29a051a3afcc5252e478149
Shin9x <andrea.sgroi01@universitadipavia.it>
2019-05-24 12:25:32 +0200

    Implementata registrazione nel nuovo client fx

--- src/main/java/it/unipv/gui/user/RegistrazioneController.java
+++ src/main/java/it/unipv/gui/user/RegistrazioneController.java
@@ -1,46 +1,118 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package it.unipv.gui.user;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
+import it.unipv.conversion.CSVToUserList;
+import it.unipv.conversion.UserToCSV;
+import it.unipv.gui.login.User;
+import it.unipv.utils.DataReferences;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author Fede
- */
 public class RegistrazioneController implements Initializable {
 
-    public Label cancel;
-    public TextField userField;
+    @FXML Label cancelLabel;
+    @FXML TextField usernameTextfield, emailTextfield;
+    @FXML PasswordField passwordTextfield, retryPasswordTextfield;
+    private List<User> users;
 
-    @FXML
-    private void cancelClose(){
-        closeStage();
-    }
-
-    @FXML
-    private void closeStage(){
-        Stage stage = (Stage) userField.getScene().getWindow();
-        stage.close();
-    }
-
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        users = CSVToUserList.getUserListFromCSV(DataReferences.USERFILEPATH);
+
+    }
+
+    @FXML
+    public void doRegister() {
+        if( usernameTextfield.getText().equalsIgnoreCase("")
+         || emailTextfield.getText().equalsIgnoreCase("")
+         || passwordTextfield.getText().equalsIgnoreCase("")
+         || retryPasswordTextfield.getText().equalsIgnoreCase("")) {
+            showAlert(Alert.AlertType.ERROR, "Errore", "Si è verificato un errore:", "Devi compilare tutti i campi!");
+        } else {
+            if(!isAlreadyThereThisUsername()) {
+                if(!isAlreadyThereThisEmail()) {
+                    if(isEmailValid()) {
+                        if(passwordTextfield.getText().equals(retryPasswordTextfield.getText())) {
+                            UserToCSV.appendUserToCSV(
+                                    new User( usernameTextfield.getText()
+                                            , passwordTextfield.getText()
+                                            , emailTextfield.getText())
+                                    , DataReferences.USERFILEPATH
+                                    , true );
+                            showAlert(Alert.AlertType.INFORMATION, "Info", "Informazione:",  "Registrazione avvenuta con successo!");
+                            doExit();
+                        } else {
+                            showAlert(Alert.AlertType.ERROR,  "Errore", "Si è verificato un errore:", "Le password non coincidono!");
+                            clearTextField(passwordTextfield, retryPasswordTextfield);
+                        }
+                    } else {
+                        showAlert(Alert.AlertType.ERROR, "Errore", "Si è verificato un errore:", "Non hai inserito una E-mail valida!");
+                        clearTextField(emailTextfield, passwordTextfield, retryPasswordTextfield);
+                    }
+                } else {
+                    showAlert(Alert.AlertType.ERROR,  "Errore", "Si è verificato un errore:", "E-mail già esistente!");
+                    clearTextField(emailTextfield, passwordTextfield, retryPasswordTextfield);
+                }
+            } else {
+                showAlert(Alert.AlertType.ERROR,  "Errore", "Si è verificato un errore:", "Nome utente già esistente!");
+                clearTextField(usernameTextfield, passwordTextfield, retryPasswordTextfield);
+            }
+        }
+    }
+
+    private boolean isAlreadyThereThisUsername() {
+        boolean status = false;
+        for(User u : users) {
+            if(u.getName().equalsIgnoreCase(usernameTextfield.getText())) {
+                status = true;
+                break;
+            }
+        }
+        return status;
+    }
+
+    private boolean isAlreadyThereThisEmail() {
+        boolean status = false;
+        for(User u : users) {
+            if(u.getEmail().equalsIgnoreCase(emailTextfield.getText())) {
+                status = true;
+                break;
+            }
+        }
+        return status;
+    }
+
+    private void showAlert(Alert.AlertType type, String title, String headerText, String message) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void clearTextField(TextField... toClear) {
+        for(TextField tf : toClear) {
+            tf.setText("");
+        }
+    }
+
+    private boolean isEmailValid() {
+        return Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+                              , Pattern.CASE_INSENSITIVE)
+                .matcher(emailTextfield.getText()).matches();
+    }
+
+
+    @FXML private void doCancel() { doExit(); }
+
+    private void doExit(){
+        Stage stage = (Stage) usernameTextfield.getScene().getWindow();
+        stage.close();
+    }
 }
